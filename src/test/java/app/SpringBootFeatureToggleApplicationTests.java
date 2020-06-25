78244b9c71caf23ed16f8aa3ef59bb35d0c1e5c2
Saúl Piña <sauljabin@gmail.com>
2020-06-25 17:10:15 -0500

    Big bang

---
+++ src/test/java/app/SpringBootFeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeatureToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
